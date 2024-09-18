e9173cb85ea28481b26906df450499ed922fab0a
Gil Matias <gilmatiasnahuel619@gmail.com>
2024-09-18 20:14:24 -0300

    actualizando portafolio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+const header = document.querySelector("header");
+
+//Evento que al bajar el scroll en el Eje Y, muestra de forma diferente el header, agregandole la clase sticky.
+window.addEventListener("scroll", ()=>{
+    header.classList.toggle('sticky', this.window.scrollY > 80);
+})
+
 let menu = document.querySelector("#menu-icon");
 let navContenedor = document.querySelector(".nav__contenedor");
 
